d192d8bbf23017c48ac06d8bdd8f2daa80731e54
boyuan zhu <uwzhuboyuan@gmail.com>
2017-10-20 16:32:43 -0400

    add some styles

--- App/Containers/Styles/HomeScreenStyle.js
+++ App/Containers/Styles/HomeScreenStyle.js
@@ -2,5 +2,14 @@ import { StyleSheet } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+	...ApplicationStyles.screen,
+	container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    marginTop: 1.5,
+    ...StyleSheet.absoluteFillObject,
+  },
 })
