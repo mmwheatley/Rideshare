9ce02cdd214965b1813c01ff60c047816d34f18b
boyuan zhu <uwzhuboyuan@gmail.com>
2017-10-20 16:32:04 -0400

    fix a bug in IOS

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { BackHandler } from 'react-native'
+import { BackHandler, Dimensions, View,StyleSheet } from 'react-native'
 import MapView from 'react-native-maps'
 import { Content, Container, Header, Left, Right, Body, Button, Text, Title, Icon, Footer, FooterTab } from 'native-base'
 import { connect } from 'react-redux'
-import Locator from '../Components/Locator'
+//import Locator from '../Components/Locator'
 import SearchBar from '../Components/SearchBar'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
@@ -12,6 +12,7 @@ import SearchBar from '../Components/SearchBar'
 import styles from './Styles/HomeScreenStyle'
 
 class HomeScreen extends Component {
+  //Detect hardware button presses for back navigation
   componentDidMount () {
     BackHandler.addEventListener('hardwareBackPress', () => {
       this.props.navigation.goBack();
@@ -19,6 +20,15 @@ class HomeScreen extends Component {
     })
   }
   render () {
+    const { width, height } = Dimensions.get('window');
+    const ratio = width / height;
+    const coordinates = {
+      latitude: 43.469750,
+      longitude: -80.542353,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0922 * ratio,
+    };
+
     return (
       <Container>
         <Header>
@@ -37,13 +47,19 @@ class HomeScreen extends Component {
 
         </Header>
 
-        <Locator/>
+        <View style={styles.container}>
+            <MapView
+              style={styles.map}
+              initialRegion={coordinates}
+            />
+        </View>
         
 
       </Container>
     )
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
