c59dc5a0fb03714e2256ef2e99f94e1cc324aa94
boyuan zhu <uwzhuboyuan@gmail.com>
2017-11-05 00:42:30 -0400

    add login v1.0

--- redux/actions/auth.js
+++ redux/actions/auth.js
@@ -1,8 +1,5 @@
 /*global fetch:true*/
-export const login = (password,email ) => {
-	    console.log('email:', email),
-        console.log('password', password),
-        console.log('login');
+export const login = (password, email) => {
     return {
         type: 'LOGIN',
         email: email,
@@ -16,43 +13,48 @@ export const logout = () => {
     };
 };
 
-export const loginUser = ( email, password ) => {
-	    console.log('email2:', email);
-        console.log('password2', password);
-        console.log('')
-  return (dispatch) => {
-  	console.log('doing')
- 	 dispatch({
-      type: 'LOAD_SPINNER'
-    });
- 	 console.log('spinner')
-    fetch('https://rideshare-carpool.herokuapp.com/users/get_token', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-              email:email,
-              password:password
-          })
-      }).then((response) => {
-        console.log(response);
-        if (response.status === 1) {
-          console.log('user_not_found!!');
-          dispatch({
-            type: 'user_not_found'
-          });
-        } else {
-          console.log('SUCCESS!!');
-          response.json().then(data => {
-            console.log(data);
-            dispatch({
-              type: 'LOGIN_USER_SUCCESS',
-              payload: data
-            });
-          });
-        }
-      });
-  };
+export const loginUser = (email, password) => {
+    console.log('email2:', email);
+    console.log('password2', password);
+    console.log('')
+    return (dispatch) => {
+        dispatch({
+            type: 'LOAD_SPINNER'
+        });
+        console.log('spinner')
+        fetch('https://rideshare-carpool.herokuapp.com/users/get_token', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }).then((response) => {
+            console.log(response);
+
+            response.json().then(data => {
+                switch (data.code) {
+                    case 1: //user_not_found
+                        console.log('user_not_found!!');
+                        dispatch({
+                            type: 'LOGIN_FAILED'
+                        });
+                        break;
+                    case 0: //no_error
+                        console.log('SUCCESS!!');
+                        console.log(data);
+                        dispatch({
+                            type: 'LOGIN_USER_SUCCESS',
+                            email: email,
+                            password: password,
+                            authentication_token: data.token
+                        });
+                        break;
+                }
+            })
+        });
+    };
 };
--- redux/reducers/auth.js
+++ redux/reducers/auth.js
@@ -9,19 +9,13 @@ const defaultState = {
 
 export default function reducer(state = defaultState, action) {
     switch (action.type) {
-        case 'LOGIN':
-            return { ...state, isLoggedIn: true, email: action.email, password: action.password }; 
         case 'LOGOUT':
-            return { ...state, isLoggedIn: false, email: action.email, password: action.password };
-            
-        case 'EMAIL_CHANGED':
-          return { ...state, email: action.payload };
-        case 'PASSWORD_CHANGED':
-          return { ...state, password: action.payload };
+            return { ...state, isLoggedIn: false, email: action.email, password: action.password }; 
+           
         case 'LOGIN_FAILED':
-          return { ...state, errorFlag: true, password: '', spinner: false };
+          return { ...state, errorFlag: true, email: '', password: '', spinner: false };
         case 'LOGIN_USER_SUCCESS':
-          return { ...state, ...action.payload, ...defaultState };
+          return { ...state, isLoggedIn: true, email: action.email, password: action.password,authentication_token:action.authentication_token};
         case 'LOAD_SPINNER':
           return { ...state, spinner: true };
         default:
