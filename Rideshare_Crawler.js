41aad59579c759443a827dad2346ba7bdd11b1e5
VQ-350 <arun.gireesan@uoit.net>
2017-11-17 01:15:59 -0500

    Module plus object acceptance

--- Rideshare_Crawler.js
+++ Rideshare_Crawler.js
@@ -5,28 +5,29 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 var Datastore = require('nedb');
+
 var db = new Datastore({ filename: 'frequent.db', autoload: true});
 
 
-var common_search = 'toronto';
-
- 
-db.insert(common_search, function(err,doc) {
+module.exports = function crawler(options) {
 
 
-console.log('Inserted', common_search , 'Id No:', doc._id);
+var origin = options.origin;
+var destination = options.destination;
+var kijiji = options.kijiji;
+var craigslist = options.boolean; 
 
+var link_array;
 
-});
 
 
 var START_URL = "https://www.kijiji.ca/b-travel-vacations/kitchener-waterloo/c302l1700212";
-var FB_URL = "https://www.facebook.com/groups/225049564330328/"
+var CRAIG_URL = "https://www.facebook.com/groups/225049564330328/"
 //var craigslist url
 
 //var SEARCH_WORD = "ride share";
 var MAX_PAGES_TO_VISIT = 10;
-var MAX_SEARCH_WORDS = 5;
+var MAX_SEARCH_WORDS = 2;
 
 
 var pagesVisited = {};
@@ -36,7 +37,7 @@ var url = new URL(START_URL);
 var baseUrl = url.protocol + "//" + url.hostname;
 
 
-var SEARCH_WORD = ["share","ride","taxi","airport","driver"];
+var SEARCH_WORD = [options.origin,options.destination];
 var word_num = 0;
 
 
@@ -100,6 +101,25 @@ function visitPage(url, callback)
      {
        console.log('Word ' + SEARCH_WORD[word_num] + ' found at page ' + url);
 
+       var city_info = {
+
+        name: SEARCH_WORD[word_num],
+        link: url
+       };
+
+
+      db.insert(city_info, function(err,doc) {
+
+
+       console.log('Inserted', city_info.name);
+
+
+        });
+
+          
+          link_array.push(city_info.name); 
+          link_array.push(city_info.url);
+
        //Increase array pointer to next word
 
        word_num++;
@@ -144,91 +164,8 @@ function collectInternalLinks($)
          pagesToVisit.push(baseUrl + $(this).attr('href'));
      });
 
-     //var absoluteLinks = $("a[href^='http']");
-//  absoluteLinks.each(function()
-    //{
-      //allAbsoluteLinks.push($(this).attr('href'));
-//  });
+
 }
 
-
-
-//fb_/craigslist crawl
-
-function fb_crawl()
-{
-  if(numPagesVisited >= MAX_PAGES_TO_VISIT)
-  {
-    console.log("Visited max number of pages");
-    return;
-  }
-
-  var nextPage = pagesToVisit.pop();
-
-  if (nextPage in pagesVisited)
-  {
-    // Crawl again if page already visited
-    fb_crawl();
-  }
-
-  else
-  {
-    // Visit the new page
-    visitPage(nextPage, crawl);
-  }
-}
-
-
-function fb_visitPage(url, callback)
-{
-  // Add page and increment num visited
-  pagesVisited[url] = true;
-  numPagesVisited++;
-
-  // Request carry out:
-  console.log("Visiting page " + url);
-  request(url, function(error, response, body)
-
-  {
-     // Retrieve status code: HTTP OK is 200
-     console.log("Status code: " + response.statusCode);
-     if(response.statusCode !== 200)
-
-     {
-       callback();
-       return;
-     }
-
-     // Document Body is parsed here
-
-     var $ = cheerio.load(body);
-     var isWordFound = searchForWord($, SEARCH_WORD[word_num]);
-
-     if(isWordFound)
-     {
-       console.log('Word ' + SEARCH_WORD[word_num] + ' found at page ' + url);
-
-       //Increase array pointer to next word
-
-       word_num++;
-
-       if (word_num < MAX_SEARCH_WORDS)
-
-        {
-          //clear pagesVisited and push start url into array
-
-          pagesVisited ={};
-          pagesToVisit.push(START_URL);
-          callback();
-        }
-
-     }
-     else
-     {
-       collectInternalLinks($);
-       // Callback is crawl()
-       callback();
-     }
-
-  });
-}
+  return link_array;
+};
